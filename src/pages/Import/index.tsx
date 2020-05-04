3195a2f237b3b05258b9fdca81fac7fa7888a51a
Francisco de Castro <franciscoescastro@gmail.com>
2020-05-03 21:15:14 -0600

    Add API integrations
    - Display balance and list all transactions on dashboard
    - Implement function to handle upload

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,41 +1,48 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-import filesize from 'filesize';
+import filesize from 'filesize'
 
-import Header from '../../components/Header';
-import FileList from '../../components/FileList';
-import Upload from '../../components/Upload';
+import Header from '../../components/Header'
+import FileList from '../../components/FileList'
+import Upload from '../../components/Upload'
 
-import { Container, Title, ImportFileContainer, Footer } from './styles';
+import { Container, Title, ImportFileContainer, Footer } from './styles'
 
-import alert from '../../assets/alert.svg';
-import api from '../../services/api';
+import alert from '../../assets/alert.svg'
+import api from '../../services/api'
 
 interface FileProps {
-  file: File;
-  name: string;
-  readableSize: string;
+  file: File
+  name: string
+  readableSize: string
 }
 
 const Import: React.FC = () => {
-  const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
-  const history = useHistory();
+  const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([])
+  const history = useHistory()
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData()
+    data.append('file', uploadedFiles[0].file)
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data)
+      setUploadedFiles([])
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error)
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const mappedFiles: FileProps[] = files.map(file => {
+      return {
+        file,
+        name: file.name,
+        readableSize: `${filesize(file.size)}`,
+      }
+    })
+    setUploadedFiles([...uploadedFiles, ...mappedFiles])
   }
 
   return (
@@ -59,7 +66,7 @@ const Import: React.FC = () => {
         </ImportFileContainer>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default Import;
+export default Import
